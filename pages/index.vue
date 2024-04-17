60fb0db9920c9d2452ac8b935cc51760a8cd8df3
kawamata <doog29suki@yahoo.co.jp>
2024-04-17 16:36:04 +0900

    add：NuxtPage追加

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<!-- @format -->
+<script setup lang="ts">
+const widthInit = Math.round(Math.random() * 10);
+const heightInit = Math.round(Math.random() * 10);
+const width = ref(widthInit);
+const height = ref(heightInit);
+const area = computed((): number => {
+  return width.value * height.value;
+});
+setInterval((): void => {
+  width.value = Math.round(Math.random() * 10);
+  height.value = Math.round(Math.random() * 10);
+}, 1000);
+</script>
+<template>
+  <h1>index Page</h1>
+  <p>縦{{ height }}で横が{{ width }}の長方形の面積は{{ area }}</p>
+</template>
