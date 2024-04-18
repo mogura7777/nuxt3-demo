2585d1c3e0c870331e3cc9597df9f406e233ee4f
kawamata <doog29suki@yahoo.co.jp>
2024-04-19 06:51:43 +0900

    fix：ステート管理修正

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <!-- @format -->
+<script setup lang="ts">
+import type { Member } from "@/interfaces";
 
+//会員情報リストをステートとして用意。
+useState<Map<number, Member>>("memberList", (): Map<number, Member> => {
+  const memberListInit = new Map<number, Member>();
+  memberListInit.set(1, {
+    id: 1,
+    name: "UseState管理",
+    email: "tttt@example.com",
+    points: 0,
+  });
+  memberListInit.set(2, {
+    id: 2,
+    name: "てんてん",
+    email: "aaaa@example.com",
+    points: 20,
+    note: "初回入会特典あり。",
+  });
+  return memberListInit;
+});
+</script>
 <template>
   <div>
     <h1>APPページ</h1>
--- pages/state.vue
+++ pages/state.vue
@@ -6,13 +6,6 @@ import type { Member } from "@/interfaces";
 //会員情報リストをステートとして用意。
 useState<Map<number, Member>>("memberList", (): Map<number, Member> => {
   const memberListInit = new Map<number, Member>();
-  memberListInit.set(33456, {
-    id: 33456,
-    name: "田中太郎",
-    email: "bow@example.com",
-    points: 35,
-    note: "初回入会特典あり。",
-  });
   memberListInit.set(47783, {
     id: 47783,
     name: "鈴木二郎",
@@ -24,7 +17,6 @@ useState<Map<number, Member>>("memberList", (): Map<number, Member> => {
 </script>
 
 <template>
-  <h1>State Page</h1>
   <NavBar />
   <StateTheBaseSection />
 </template>
