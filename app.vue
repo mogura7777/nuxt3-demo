c49b47fa0ef56e04339beefd9cdaf627a6bac469
kawamata <doog29suki@yahoo.co.jp>
2024-04-19 16:26:17 +0900

    fix：天気useState追加

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <!-- @format -->
 <script setup lang="ts">
 import type { Member } from "@/interfaces";
+import type { City } from "@/interfaces";
 const SITE_TITLE = "Nuxt3デモ";
-
 useHead({
   titleTemplate: (titleChunk: string | undefined): string => {
     let title = SITE_TITLE;
@@ -13,6 +13,27 @@ useHead({
   },
   // titleTemplate: `%s | ${SITE_TITLE}`
 });
+//都市情報リストをステートとして用意。
+useState<Map<number, City>>("cityList", (): Map<number, City> => {
+  const cityListInit = new Map<number, City>();
+  cityListInit.set(1853909, {
+    id: 1853909,
+    name: "大阪",
+    q: "Osaka",
+  });
+  cityListInit.set(1859171, {
+    id: 1859171,
+    name: "神戸",
+    q: "Kobe",
+  });
+  cityListInit.set(1862627, {
+    id: 1862627,
+    name: "姫路",
+    q: "Himeji",
+  });
+  return cityListInit;
+});
+
 //会員情報リストをステートとして用意。
 useState<Map<number, Member>>("memberList", (): Map<number, Member> => {
   const memberListInit = new Map<number, Member>();
